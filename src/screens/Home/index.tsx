af0454a866aa291dde1c9da39bfca697ec3b7924
Luiz Timbó <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-11-22 23:07:07 -0300

    Tela de detalhes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,10 +6,10 @@ import { Car } from "../../components/Car";
 
 import Logo from "../../assets/logo.svg";
 
-import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
 
 export function Home() {
-  const carDataOne = {
+  const carData = {
     brand: "Audi",
     name: "Rs 5 Coupé",
     rent: {
@@ -17,16 +17,6 @@ export function Home() {
       price: 120,
     },
     thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
-  };
-  const carDataTwo = {
-    brand: "Porsche",
-    name: "Panamera",
-    rent: {
-      period: "Ao dia",
-      price: 340,
-    },
-    thumbnail:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcjNyYzAaDuKhvsO_t45BZbB4uYhtqryrA5efIsnfaGWsU-P3r05g-X34M09z9vXVSUbs&usqp=CAU",
   };
 
   return (
@@ -43,8 +33,11 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
-      <Car data={carDataOne} />
-      <Car data={carDataTwo} />
+      <CarList
+        keyExtractor={(item) => String(item)}
+        data={[1, 2, 3, 4, 5, 6, 7]}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 }
