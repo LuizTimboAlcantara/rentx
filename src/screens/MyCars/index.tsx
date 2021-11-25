62a34250c45f2b6e8552e887a2a3409ab8fea387
Luiz Timbó <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-11-25 11:33:57 -0300

    Exibindo carros agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,13 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { StatusBar, FlatList } from "react-native";
+import { CommonActions, useNavigation, useRoute } from "@react-navigation/native";
 
 import { CarDTO } from "../../dtos/carDTO";
 import api from "../../services/api";
 
-import { Container } from "./styles";
+import { BackButton } from "../../components/BackButton";
+import { Car } from "../../components/Car";
+
+import { useTheme } from "styled-components";
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+} from "./styles";
+
+interface CarProps {
+  id: string;
+  car: CarDTO;
+  user_id: string;
+}
 
 export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const navigation = useNavigation();
+  const route = useRoute();
+  const theme = useTheme();
+
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
+
+  function handleBack() {
+    navigation.dispatch(CommonActions.goBack());
+  }
 
   useEffect(() => {
     async function fetchCars() {
@@ -24,5 +53,33 @@ export function MyCars() {
     fetchCars();
   }, []);
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+
+        <Title>{`Escolha uma ${"\n"} data de início e ${"\n"} fim do aluguel`}</Title>
+        <SubTitle>"Conforto, segurança e praticidade."</SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>5</AppointmentsQuantity>
+        </Appointments>
+
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={cars}
+          renderItem={({ item }) => <Car data={item.car} />}
+          showsVerticalScrollIndicator={false}
+        />
+      </Content>
+    </Container>
+  );
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,3 +1,57 @@
 import styled from "styled-components/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(325)}px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: center;
+  padding: ${RFValue(25)}px;
+  padding-top: ${getStatusBarHeight() + RFValue(30)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(30)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.shape};
+  margin-top: ${RFValue(24)}px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.secondary_400};
+  color: ${({ theme }) => theme.colors.shape};
+  margin-top: ${RFValue(24)}px;
+`;
+export const Content = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 0 ${RFValue(16)}px;
+`;
+
+export const Appointments = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${RFValue(24)}px;
+`;
+
+export const AppointmentsTitle = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const AppointmentsQuantity = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  color: ${({ theme }) => theme.colors.title};
+`;
