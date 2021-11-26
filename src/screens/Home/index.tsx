a83069299374a077705d542b3ca4a6a425a49feb
Luiz Timbó <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-11-26 12:43:14 -0300

    Previnindo volta a tela de splash

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, StyleSheet, BackHandler } from "react-native";
 import { useTheme } from "styled-components";
 import { CommonActions, useNavigation } from "@react-navigation/native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Ionicons } from "@expo/vector-icons";
+import { RectButton, PanGestureHandler } from "react-native-gesture-handler";
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
+} from "react-native-reanimated";
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 import api from "../../services/api";
 import { CarDTO } from "../../dtos/carDTO";
@@ -27,6 +36,30 @@ export function Home() {
   const theme = useTheme();
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+
+  const myCarButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: positionX.value }, { translateY: positionY.value }],
+    };
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any) {
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any) {
+      positionX.value = event.translationX + event.translationX;
+      positionY.value = event.translationY + event.translationY;
+    },
+    onEnd() {
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    },
+  });
 
   function handleCarDetails(car: CarDTO) {
     navigation.dispatch(
@@ -63,6 +96,13 @@ export function Home() {
     fetchCars();
   }, []);
 
+  useEffect(() => {
+    //! Android -> Não voltar para a tela de splash;
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      return true;
+    });
+  }, []);
+
   return (
     <Container>
       <StatusBar
@@ -74,7 +114,7 @@ export function Home() {
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
 
-          <TotalCars>Total de {cars.length} carros</TotalCars>
+          {!loading && <TotalCars>Total de {cars.length} carros</TotalCars>}
         </HeaderContent>
       </Header>
       {loading ? (
@@ -89,14 +129,28 @@ export function Home() {
         />
       )}
 
-      <MyCarButton>
-        <Ionicons
-          name="ios-car-sport"
-          size={32}
-          color={theme.colors.shape}
-          onPress={handleOpenMyCar}
-        />
-      </MyCarButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[myCarButtonStyle, { position: "absolute", bottom: 13, right: 22 }]}
+        >
+          <ButtonAnimated
+            onPress={handleOpenMyCar}
+            style={[styles.button, { backgroundColor: theme.colors.main }]}
+          >
+            <Ionicons name="ios-car-sport" size={32} color={theme.colors.shape} />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
